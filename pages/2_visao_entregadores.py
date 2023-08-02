4c03a3d35b45408fd4ebecf02978fe826ac6d6fe
Thiago Negrão Leite <105130482+tnleite@users.noreply.github.com>
2023-08-02 16:43:27 -0300

    Update 2_visao_entregadores.py

--- pages/2_visao_entregadores.py
+++ pages/2_visao_entregadores.py
@@ -8,7 +8,7 @@ import folium
 from streamlit_folium import folium_static
 import streamlit as st
 
-st.set_page_config(page_title="Visão Restaurantes", page_icon="🍽️", layout="wide")
+st.set_page_config(page_title="Visão entregadores", page_icon="🚚", layout="wide")
 
 #Importação do arquivo
 df_raw = pd.read_csv('dataset/train.csv')
